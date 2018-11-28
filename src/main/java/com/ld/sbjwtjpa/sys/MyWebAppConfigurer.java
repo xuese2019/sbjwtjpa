a0e933717359286fbfa02ba09a9bcdbe473d6f34
shengzhizhe <15100162730@163.com>
2018-11-28 10:49:24 +0800

    首次

--- src/main/java/com/ld/sbjwtjpa/sys/MyWebAppConfigurer.java
+++ src/main/java/com/ld/sbjwtjpa/sys/MyWebAppConfigurer.java
@@ -1,11 +1,13 @@
 package com.ld.sbjwtjpa.sys;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.support.GenericConversionService;
 import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 import javax.annotation.PostConstruct;
@@ -14,21 +16,10 @@ import javax.annotation.PostConstruct;
  * 跨域配置
  */
 @Configuration
-public class MyWebAppConfigurer implements WebMvcConfigurer {
+public class MyWebAppConfigurer {
 
     @Autowired
     private RequestMappingHandlerAdapter handlerAdapter;
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-//        允许的方法地址 /api/**
-        registry.addMapping("**")
-//                允许的ip地址 http://192.168.1.97
-                .allowedOrigins("**")
-//                允许的请求方式 "get","post"
-                .allowedMethods("*")
-                .allowCredentials(false).maxAge(3600);
-    }
 
     /**
      * 字符串转日期
@@ -40,6 +31,21 @@ public class MyWebAppConfigurer implements WebMvcConfigurer {
             GenericConversionService genericConversionService = (GenericConversionService) initializer.getConversionService();
             genericConversionService.addConverter(new StringToDateConverter());
         }
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // 1允许任何域名使用
+        corsConfiguration.addAllowedHeader("*"); // 2允许任何头
+        corsConfiguration.addAllowedMethod("*"); // 3允许任何方法（post、get等）
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
     }
 
 
