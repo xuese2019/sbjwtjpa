f6030a59b1721c04e3c5fe16123f67aa51f43632
xuesemofa <xuesemofa@126.com>
2018-12-01 12:54:56 +0800

    格式化代码

--- src/main/java/com/ld/sbjwtjpa/business/sys/organization/model/OrganizationModel.java
+++ src/main/java/com/ld/sbjwtjpa/business/sys/organization/model/OrganizationModel.java
@@ -3,6 +3,7 @@ package com.ld.sbjwtjpa.business.sys.organization.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Table;
 
@@ -20,6 +21,7 @@ import java.util.List;
  * @remarks
  */
 @ApiModel(value = "OrganizationModel", description = "组织机构表")
+@Data
 @Entity(name = "organization_table")
 @Table(comment = "组织机构表", appliesTo = "organization_table")
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -30,91 +32,28 @@ public class OrganizationModel implements Serializable {
     @GeneratedValue(generator = "system-uuid")
     private String uuid;
 
-    //    组织机构名称
+    /**
+     * 组织机构名称
+     */
     @ApiModelProperty(value = "组织机构名称", name = "orgName", dataType = "String", required = true, allowableValues = "组织机构名称")
     @NotBlank(message = "组织机构名称不能为空")
     @Size(max = 200, message = "组织机构名称最大长度为200位")
     @Column(name = "org_name", nullable = false)
     private String orgName;
-    //    组织机构父级
+    /**
+     * 组织机构父级
+     */
     @ApiModelProperty(value = "组织机构父级", name = "orgParent", dataType = "String", required = true, allowableValues = "组织机构父级,第一级用0标识")
     @NotBlank(message = "组织机构父级不能为空")
     @Size(max = 200, message = "组织机构父级最大长度为200位")
     @Column(name = "org_parent", nullable = false)
     private String orgParent;
-    //数据库中不创建该字段
+    /**
+     * 数据库中不创建该字段
+     */
     @Transient
     private List<OrganizationModel> list = new ArrayList<>();
 
     @Version
     private Long version;
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getOrgName() {
-        return orgName;
-    }
-
-    public void setOrgName(String orgName) {
-        this.orgName = orgName;
-    }
-
-    public String getOrgParent() {
-        return orgParent;
-    }
-
-    public void setOrgParent(String orgParent) {
-        this.orgParent = orgParent;
-    }
-
-    public Long getVersion() {
-        return version;
-    }
-
-    public void setVersion(Long version) {
-        this.version = version;
-    }
-
-    public List<OrganizationModel> getList() {
-        return list;
-    }
-
-    public void setList(List<OrganizationModel> list) {
-        this.list = list;
-    }
-
-    public OrganizationModel() {
-        super();
-    }
-
-    public OrganizationModel(String uuid, @NotBlank(message = "组织机构名称不能为空") @Size(max = 200, message = "组织机构名称最大长度为200位") String orgName, @NotBlank(message = "组织机构父级不能为空") @Size(max = 200, message = "组织机构父级最大长度为200位") String orgParent, Long version) {
-        this.uuid = uuid;
-        this.orgName = orgName;
-        this.orgParent = orgParent;
-        this.version = version;
-    }
-
-    public OrganizationModel(String uuid, @NotBlank(message = "组织机构名称不能为空") @Size(max = 200, message = "组织机构名称最大长度为200位") String orgName, @NotBlank(message = "组织机构父级不能为空") @Size(max = 200, message = "组织机构父级最大长度为200位") String orgParent, List<OrganizationModel> list, Long version) {
-        this.uuid = uuid;
-        this.orgName = orgName;
-        this.orgParent = orgParent;
-        this.list = list;
-        this.version = version;
-    }
-
-    @Override
-    public String toString() {
-        return "OrganizationModel{" +
-                "uuid='" + uuid + '\'' +
-                ", orgName='" + orgName + '\'' +
-                ", orgParent='" + orgParent + '\'' +
-                ", version=" + version +
-                '}';
-    }
 }
--- src/main/java/com/ld/sbjwtjpa/index/controller/IndexController.java
+++ src/main/java/com/ld/sbjwtjpa/index/controller/IndexController.java
@@ -2,7 +2,6 @@ package com.ld.sbjwtjpa.index.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
