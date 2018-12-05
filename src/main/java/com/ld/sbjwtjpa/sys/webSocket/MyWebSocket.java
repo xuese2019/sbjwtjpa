be9fa8955a2609dd2c7a86dbae6ccb848449a9a4
shengzhizhe <15100162730@163.com>
2018-12-05 16:54:50 +0800

    添加webSocket相关配置

---
+++ src/main/java/com/ld/sbjwtjpa/sys/webSocket/MyWebSocket.java
@@ -0,0 +1,15 @@
+package com.ld.sbjwtjpa.sys.webSocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Configuration
+//@Component
+public class MyWebSocket {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
